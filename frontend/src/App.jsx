6c843b845c2cd978de278b1e5c344e7e67130671
TinYuetKo2007 <kotinyuet@gmail.com>
2026-03-17 22:37:57 +0000

    small changes to landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,8 +22,14 @@ function App() {
 </div>
 
     <div className='second-section'>
-    <h2>Trending products</h2>
-    <p>product scroll goes here</p>
+    <h1>About Greenfield Local Hub</h1>
+    <p>We are a co-operative of local farmers and food producers in the United Kingdom. 
+      We hope to offer the best locally-produced food and drinks.</p>
+    <h2>By using our online App, you can:</h2>
+    <ul>
+      <li>Buy locally produced food, sourced from our farms</li><br/>
+      <li>Learn more about the benefits of buying from local sources</li><br/>
+    </ul>
     </div>
     
     </div>
